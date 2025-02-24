4333fd31ec12818d8db44f842522410fc3bb7771
billdineen03 <billdineen@gmail.com>
2025-02-24 11:27:53 -0500

    Create initial headings to snippets function

---
+++ headings_to_principles/headings_to_principles.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+
+with open('test_sheet.csv') as f:
+    output = []
+    reader = csv.reader(f)
+    for i, row in enumerate(reader):
+        if i == 0:
+            continue
+        book_title = row[0]
+        heading = row[1]
+        snippets = row[2]
+        if row[5] == 'y':
+            correct = row[3]
+        else:
+            correct = row[7]
+        principles = correct.split('- ')
+        for principle in principles:
+            if principle:
+                output.append({'book_title': book_title, 'heading': heading, 'snippets': snippets, 'principle': principle.strip()})
+
+      
+with open('test_output.csv', 'w', newline='') as csvfile:
+    fieldnames = ['book_title', 'heading', 'snippets', 'principle']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(output)
+
