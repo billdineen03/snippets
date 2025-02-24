a0bac62aed6eac93580e1c4656ea6b8bf8f9968d
billdineen03 <billdineen@gmail.com>
2025-02-24 13:45:42 -0500

    Fix valid validity test so it works now

--- headings_to_principles/headings_to_principles.py
+++ headings_to_principles/headings_to_principles.py
@@ -7,24 +7,22 @@ with open('test_sheet.csv') as f:
     for i, row in enumerate(reader):
         if i == 0:
             continue
-        try:
-            row[5] == 'y' or row[5] == 'n' or row[5] == 'x'
-        except:
-            print(f"Invalid validity status in row {i+1}")
-        else:
-            if row[5] == 'x':
+        if row[5] == 'x':
                 continue
-            book_title = row[0]
-            heading = row[1]
-            snippets = row[2]
-            if row[5] == 'y':
-                correct = row[3]
-            elif row[5] == 'n':
-                correct = row[7]
-            principles = correct.split('- ')
-            for principle in principles:
-                if principle and principle != "None":
-                    output.append({'book_title': book_title, 'heading': heading, 'principle': principle.strip(), 'snippets': snippets})
+        elif row[5] == 'y':
+            correct = row[3]
+        elif row[5] == 'n':
+            correct = row[7]
+        else:
+            raise Exception(f"Invalid validity status in row {i+1}")
+        book_title = row[0]
+        heading = row[1]
+        snippets = row[2]
+        
+        principles = correct.split('- ')
+        for principle in principles:
+            if principle and principle != "None":
+                output.append({'book_title': book_title, 'heading': heading, 'principle': principle.strip(), 'snippets': snippets})
 
       
 with open('test_output.csv', 'w', newline='') as csvfile:
